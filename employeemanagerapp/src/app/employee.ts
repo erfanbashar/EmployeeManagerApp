82962f35c64da07910700ee8d246df4115f5ba24
erfanbashar <erfan.bashar.13@gmail.com>
2022-01-06 00:37:18 +0600

    new add

---
+++ employeemanagerapp/src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    id: number;
+    name: string;
+    email: string;
+    age: number;
+    jobTitle: string;
+    phoneNumber: string;
+    imageUrl: string;
+    employeeCode: string;
+    location: string;
+}
